ab9872167f74bb81027b2fe8477aa8ed63c43d04
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-08-31 20:28:35 +0100

    Setup verifyEmail route and its controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -33,6 +33,28 @@ const register = async (req, res) => {
     });
 };
 
+const verifyEmail = async (req, res) => {
+    const {verificationToken, email} = req.body;
+    const user = await User.findOne({email});
+
+    if (!user) {
+        throw new CustomError.UnauthenticatedError('Verification Failed!');
+    }
+
+    if (user.verificationToken !== verificationToken) {
+        throw new CustomError.UnauthenticatedError('Verification Failed!');
+    }
+
+    (user.isVerified = true), (user.verified = Date.now());
+    user.verificationToken = '';
+
+    await user.save();
+
+    res.status(StatusCodes.OK).json({
+        msg: 'Email Verified!',
+    });
+};
+
 const login = async (req, res) => {
     const {email, password} = req.body;
 
@@ -60,6 +82,7 @@ const login = async (req, res) => {
 
     res.status(StatusCodes.OK).json({user: tokenUser});
 };
+
 const logout = async (req, res) => {
     res.cookie('token', 'logout', {
         httpOnly: true,
@@ -72,4 +95,5 @@ module.exports = {
     register,
     login,
     logout,
+    verifyEmail,
 };
