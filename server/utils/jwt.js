c5d5c64e11dea48bb82004e555b4b8f0d1f4e051
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-09-02 18:19:03 +0100

    Update createJWT utility function

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -1,9 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const createJWT = ({payload}) => {
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_LIFETIME,
-    });
+    const token = jwt.sign(payload, process.env.JWT_SECRET);
     return token;
 };
 
