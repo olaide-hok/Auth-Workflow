fc0edff31c903224b6d35df44a9c0a3d3b257943
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-09-04 19:19:27 +0100

    Format file

--- server/app.js
+++ server/app.js
@@ -30,10 +30,10 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.set('trust proxy', 1);
 app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000,
-    max: 60,
-  })
+    rateLimiter({
+        windowMs: 15 * 60 * 1000,
+        max: 60,
+    })
 );
 app.use(helmet());
 app.use(cors());
@@ -57,14 +57,14 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URL);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await connectDB(process.env.MONGO_URL);
+        app.listen(port, () =>
+            console.log(`Server is listening on port ${port}...`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 start();
